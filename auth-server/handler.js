c4486aa6053fce08cb0c873173ed394ea5fd42ec
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-03-05 17:32:20 +0100

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -98,9 +98,6 @@ module.exports.getAccessToken = async (event) => {
       console.error(err);
       return {
         statusCode: 500,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
         body: JSON.stringify(err),
       };
     });
