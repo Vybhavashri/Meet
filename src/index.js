75a070bf59ac948d47d323cbf7c6d93160c9cb80
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-03-14 20:58:25 +0100

    Update index.js
    Adding Atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('e3f594798d944bbb9aeeccead744926e').install();
 
 ReactDOM.render(
   <React.StrictMode>
