8da7132bd03f3ffa52d1ac46cdafd1ea31e27023
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-03-16 16:56:18 +0100

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
+import { Container, Row, Col } from "react-bootstrap";
 
 class NumberOfEvents extends Component {
   state = {
     numberOfEvents: 32,
+    infoText: '',
   }
 
   handleInputChange = (event) => {
     const value = event.target.value;
-    this.setState({
-      numberOfEvents: value,
-    });
+    if (value > 32 || value < 1) {
+      this.setState({
+        infoText: 'Select numbers from 1 to 32'
+      });
+    } else {
+      this.setState({
+        numberOfEvents: value,
+        infoText: '',
+      });
+    }
     this.props.updateNumberOfEvents(event.target.value);
   };
 
   render() {
     return (
-      <div className="NumberOfEvents">
-        <input
-          type="number"
-          className="numberOfEvents"
-          value={this.props.numberOfEvents}
-          onChange={(e) => this.props.updateNumberOfEvents(e)}
-        />
-      </div>
+      <Container className="NumberOfEvents">
+        <Row>
+          <Col>
+            <p>Number of events:</p>
+            <input
+              type="number"
+              className="numberOfEvents"
+              value={this.state.numberOfEvents}
+              onChange={this.handleInputChange}
+            />
+          </Col>
+          <Col>
+            <ErrorAlert text={this.state.infoText} />
+          </Col>
+        </Row>
+      </Container>
     )
   }
 };
