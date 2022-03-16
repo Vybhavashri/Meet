23328b2d0cf5360654aa10712041bf4584f59ec4
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-03-16 16:55:58 +0100

    Create Alert.js

---
+++ src/Alert.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Alert extends Component {
+  constructor(props) {
+    super(props);
+    this.color = null;
+    this.fontSize = 10;
+  }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+      fontSize: this.fontSize,
+    };
+  }
+
+  render() {
+    return (
+      <div className="Alert">
+        <p style={this.getStyle()}>{this.props.text}</p>
+      </div>
+    );
+  }
+}
+
+export class InfoAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'blue';
+  }
+}
+
+export class ErrorAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'red';
+  }
+}
