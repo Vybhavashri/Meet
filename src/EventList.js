bda9b2d36df2cf3854fe19cf5bdfb06a493f83bf
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-03-18 19:47:13 +0100

    Update EventList.js

--- src/EventList.js
+++ src/EventList.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
 import Event from './Event';
-import { OfflineAlert } from './Alert';
 
 class EventList extends Component {
   render() {
     const { events } = this.props;
     return (
-      <Container className="eventlist-container">
-        {!navigator.onLine ? (
-          <OfflineAlert text="You are offline! The displayed event list has been loaded from the cache." />
-        ) : (
-          <OfflineAlert text="" />
+      <ul className='EventList'>
+        {events.map(event =>
+          <li key={event.id}>
+            <Event event={event} />
+          </li>
         )}
-        <Row className="d-flex justify-content-center eventlist">
-          {events.map((event) => (
-            <Col sm={12} md={6} lg={4} key={event.id}>
-              <Event event={event} />
-            </Col>
-          ))}
-        </Row>
-      </Container>
+      </ul>
     );
   }
 }
