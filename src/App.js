d5de79543e4e2b6028c17790d2d3beda3daf8e50
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-03-19 02:20:02 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
+import { getEvents, extractLocations } from './api';
 import { Container, Row, Col } from "react-bootstrap";
-import WelcomeScreen from "./WelcomeScreen";
 
 class App extends Component {
 
@@ -14,37 +13,20 @@ class App extends Component {
     locations: [],
     currentLocation: 'all',
     numberOfEvents: 32,
-    showWelcomeScreen: undefined,
     OfflineAlertText: '',
   }
 
-  async componentDidMount() {
+  componentDidMount() {
+    const { numberOfEvents } = this.state.numberOfEvents;
     this.mounted = true;
-    const accessToken = localStorage.getItem("access_token");
-    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get("code");
-
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
-      getEvents().then((events) => {
-        if (this.mounted) {
-          this.setState({
-            events: events.slice(0, this.state.numberOfEvents),
-            locations: extractLocations(events),
-          });
-        }
-      });
-    }
-    if (!navigator.onLine) {
-      this.setState({
-        OfflineAlertText: 'You are currently offline, events may not be updated.'
-      })
-    } else {
-      this.setState({
-        OfflineAlertText: ''
-      })
-    }
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({
+          events: events.slice(0, numberOfEvents),
+          locations: extractLocations(events)
+        });
+      }
+    });
   }
 
   componentWillUnmount() {
@@ -53,16 +35,13 @@ class App extends Component {
 
   updateEvents = (location) => {
     getEvents().then((events) => {
-      const locationEvents = (location === 'all')
-        ? events
-        : events.filter((event) => event.location === location);
-      const { numberOfEvents } = this.state.numberOfEvents;
-      if (this.mounted) {
-        this.setState({
-          events: locationEvents.slice(0, numberOfEvents),
-          currentLocation: location,
-        });
-      }
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents.slice(0, this.state.numberOfEvents),
+        currentLocation: location,
+      });
     });
   }
 
@@ -100,12 +79,6 @@ class App extends Component {
         </Row>
         <Row>
           <Col>
-            <WelcomeScreen
-              showWelcomeScreen={this.state.showWelcomeScreen}
-              getAccessToken={() => {
-                getAccessToken();
-              }}
-            />
           </Col>
         </Row>
       </Container>
