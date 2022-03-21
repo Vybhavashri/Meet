125a7d2e285461b56c437025bd0f6cbffdac4c8e
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-03-21 20:56:59 +0100

    Update EventList.js

--- src/EventList.js
+++ src/EventList.js
@@ -1,32 +1,12 @@
 import React, { Component } from 'react';
 import Event from './Event';
 import { Container, Row, Col } from "react-bootstrap";
-import { WarningAlert } from './Alert';
 
 class EventList extends Component {
-  state = {
-    warningText: '',
-  }
-
-  componentDidMount() {
-    if (!navigator.onLine) {
-      this.setState({
-        warningText: 'You are offline! The displayed event list is loaded from the cache.'
-      })
-    }
-    else {
-      this.setState({
-        warningText: ''
-      })
-    }
-  }
-
   render() {
     const { events } = this.props;
     return (
       <Container className="eventlist-container">
-        <br /><br />
-        <WarningAlert className="Alert" text={this.state.warningText} />
         <Row className="d-flex justify-content-center eventlist">
           {events.map((event) => (
             <Col sm={12} md={6} lg={4} key={event.id}>
